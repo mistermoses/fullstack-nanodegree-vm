9117b4a8104dbe12d7d93c270d24fe113ebc7070
Dan Moses <dmoses.radioflag@gmail.com>
2015-10-27 12:33:30 -0700

    Finished first database project--basic forum

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,11 +1,14 @@
 #
 # Database access functions for the web forum.
-# 
+#
 
+import psycopg2
 import time
+import bleach
 
 ## Database connection
-DB = []
+def ConnectDB():
+    return psycopg2.connect("dbname=forum");
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,9 +19,13 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    db = ConnectDB()
+    c = db.cursor()
+    c.execute("SELECT time, content FROM posts ORDER BY time DESC;")
+    posts = ({'content': str(bleach.clean(row[1])), 'time':str(row[0])} for row in c.fetchall())
+    db.close()
     return posts
+
 
 ## Add a post to the database.
 def AddPost(content):
@@ -27,5 +34,8 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    db = ConnectDB()
+    c = db.cursor()
+    c.execute("INSERT INTO posts (content) values (%s)", (bleach.clean(content),))
+    db.commit()
+    db.close()
